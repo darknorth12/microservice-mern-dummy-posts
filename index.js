644c032e1130359e453c21aaed9eba3e852eaf3c
Ravi Ranwa <ravi.ranwa@outlook.com>
2020-12-13 11:44:00 +0530

    event api added

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const { randomBytes } = require("crypto");
 const bodyParser = require("body-parser");
 const cors = require('cors');
+const axios = require('axios');
 
 const app = express();
 app.use(bodyParser.json());
@@ -11,18 +12,30 @@ const posts = {};
 
 const PORT = 4000;
 
-app.post("/posts", (req, res) => {
+app.post("/posts", async (req, res) => {
   const id = randomBytes(4).toString("hex");
   const { title } = req.body;
   posts[id] = {
     id,
     title,
   };
+  await axios.post("http://localhost:4005/events",{
+    type : "PostCreated",
+    data : {
+      id,title
+    }
+  })
+  
   res.status(201).send(posts[id]);
 });
 
 app.get("/posts", (req, res) => {
   res.send(posts);
+});
+
+app.post("/events", (req, res) => {
+  console.log("Event Received: ", req.body.type);
+  res.send({});
 });
 
 app.listen(PORT, () => {
